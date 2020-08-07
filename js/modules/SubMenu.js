c0260a95907287ee5373d9d21fef1c01a46c8b69
Eduardo9976 <60363926+Eduardo9976@users.noreply.github.com>
2020-08-07 12:44:52 -0300

    ajustando menu

--- js/modules/SubMenu.js
+++ js/modules/SubMenu.js
@@ -2,28 +2,34 @@ export default class SubMenu {
   constructor (submenuOne, submenuTwo) {
     this.containerSubmenuOne = document.querySelector(submenuOne);
     this.containerSubmenuTwo = document.querySelector(submenuTwo);
-    this.ref = document.querySelectorAll(submenuTwo);
-
-    this.handleClick = this.handleClick.bind(this);
-    this.handleSecondClick = this.handleSecondClick.bind(this);
-    }
-
-  addEvents () {
-    this.containerSubmenuOne.addEventListener('click', this.handleClick);
+    this.body = document.querySelector('body')
+    this.leaveSubmenu = {
+      one: '',
+      two: '',
+      body: ''
+    } 
   }
-  handleClick () {
-    event.target.classList.toggle('active');
-    event.target.addEventListener('click', this.handleSecondClick);
+
+  addEvents() {
+    this.containerSubmenuOne.addEventListener('click', () => {
+      this.leaveSubmenu.one = event.currentTarget
+      this.containerSubmenuOne.classList.add('active')
+      this.eventBody()
+
+      this.containerSubmenuTwo.addEventListener('click', () => {
+        this.containerSubmenuTwo.classList.toggle('active')
+        this.leaveSubmenu.two = event.currentTarget
+      })
+    })
   }
-  handleSecondClick () {   
-    const body = document.querySelector('body')
-    body.addEventListener('click', (event) => {
-        if (event.target === this.containerSubmenuOne || event.target === this.containerSubmenuTwo) { 
-        } else {
-            this.containerSubmenuOne.classList.remove('active')
-            this.containerSubmenuTwo.classList.remove('active')
-        }
-     })
+  eventBody() {
+    this.body.addEventListener('click', () => {
+      this.leaveSubmenu.body = event.target
+      if ( this.leaveSubmenu.body != this.leaveSubmenu.one  && this.leaveSubmenu.body != this.leaveSubmenu.two){
+        this.containerSubmenuOne.classList.remove('active')
+        this.containerSubmenuTwo.classList.remove('active')
+      } 
+    })
   }
   init() {
     if (this.containerSubmenuOne && this.containerSubmenuTwo)
